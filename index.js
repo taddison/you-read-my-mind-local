e156a39f1d1bd2e278f4dcaa32d77cb5d8f0bcf7
Tim Addison <git@tjaddison.com>
2020-04-26 10:52:08 -0400

    sessions inside of game

--- index.js
+++ index.js
@@ -6,33 +6,24 @@ const app = express();
 app.use(bodyparser.json());
 app.use(morgan("dev"));
 
-let sessions = [];
-let games = [{ gameId: 89 }];
+let games = [{ gameId: 89, sessions: [] }];
 
 app.post("/createsession", (req, res, next) => {
   const { name, gameId, sessionId } = req.body;
 
-  sessions.push({ name, gameId: parseInt(gameId), sessionId });
+  const game = games.find((g) => g.gameId === parseInt(gameId));
+  game.sessions.push({ sessionId, name });
 
   res.status(201).end();
 });
 
-app.post("/getsessions", (req, res, next) => {
-  console.table(sessions);
-  const { gameId } = req.body;
-
-  const sessionsToReturn = sessions.filter(
-    (s) => s.gameId === parseInt(gameId)
-  );
-  res.json(sessionsToReturn);
-});
-
 app.post("/deletesession", (req, res, next) => {
   let { sessionId, gameId } = req.body;
-  gameId = parseInt(gameId);
-  sessions = sessions.filter(
-    (s) => !(s.sessionId === sessionId && s.gameId === gameId)
-  );
+
+  const game = games.find((g) => g.gameId === parseInt(gameId));
+  console.log(game);
+  game.sessions = game.sessions.filter((s) => s.sessionId !== sessionId);
+
   res.status(204).end();
 });
 
@@ -68,8 +59,10 @@ app.post("/setgame", (req, res, next) => {
 app.post("/getgame", (req, res, next) => {
   console.table(games);
   const { gameId } = req.body;
-
+  
   const game = games.find((g) => g.gameId === parseInt(gameId));
+  console.table(game.sessions);
+
   res.json(game);
 });
 
